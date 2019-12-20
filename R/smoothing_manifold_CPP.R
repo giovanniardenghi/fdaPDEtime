f69e65b8a8a378164cf423146d6fcacde2e1bdb8
andrea <andrea1.vicini@mail.polimi.it>
2019-12-20 23:18:05 +0100

    Some modifications for the case when location is empty

--- R/smoothing_manifold_CPP.R
+++ R/smoothing_manifold_CPP.R
@@ -1,40 +1,73 @@
-CPP_smooth.manifold.FEM.basis<-function(locations, observations, FEMbasis, lambda, covariates = NULL, incidence_matrix = NULL, ndim, mydim, BC = NULL, GCV,GCVMETHOD = 2, nrealizations = 100)
+CPP_smooth.manifold.FEM.basis<-function(locations, time_locations, observations, FEMbasis,time_mesh, lambdaS, lambdaT, PDE_parameters,
+                                    covariates, incidence_matrix, ndim, mydim, BC, FLAG_MASS, FLAG_PARABOLIC, IC,GCV, GCVMETHOD, nrealizations)
 {
-  
+
   # C++ function for manifold works with vectors not with matrices
-  
+
   FEMbasis$mesh$triangles=c(t(FEMbasis$mesh$triangles))
   FEMbasis$mesh$nodes=c(t(FEMbasis$mesh$nodes))
-  
+
   # Indexes in C++ starts from 0, in R from 1, opportune transformation
- 
+
   FEMbasis$mesh$triangles=FEMbasis$mesh$triangles-1
-  
-  
+
+
   if(is.null(covariates))
   {
     covariates<-matrix(nrow = 0, ncol = 1)
   }
-  
+
   if(is.null(locations))
   {
     locations<-matrix(nrow = 0, ncol = ndim)
   }
-  
+
   if(is.null(incidence_matrix))
   {
     incidence_matrix<-matrix(nrow = 0, ncol = 1)
   }
-  
+
+  if(is.null(IC))
+  {
+    IC<-matrix(nrow = 0, ncol = 1)
+  }
+
+  if(is.null(time_locations))
+  {
+    if(FLAG_PARABOLIC==FALSE)
+    {
+      time_locations<-time_mesh
+    }
+    else
+    {
+      NumTimeInstants = length(time_mesh)
+      time_locations<-time_mesh[1:NumTimeInstants-1]
+    }
+  }
+
+  if(is.null(time_mesh))
+  {
+    if(FLAG_PARABOLIC==FALSE)
+    {
+      time_mesh<-time_locations
+    }
+    else
+    {
+      NumTimeInstants = length(time_locations)
+      lastTimeDiff = time_locations[NumTimeInstants]-time_locations[NumTimeInstants-1]
+      time_mesh<-c(time_locations,time_locations[NumTimeInstants]+lastTimeDiff)
+    }
+  }
+
   if(is.null(BC$BC_indices))
   {
     BC$BC_indices<-vector(length=0)
   }else
-  { 
+  {
     BC$BC_indices<-as.vector(BC$BC_indices)-1
-    
+
   }
-  
+
   if(is.null(BC$BC_values))
   {
     BC$BC_values<-vector(length=0)
@@ -42,7 +75,7 @@ CPP_smooth.manifold.FEM.basis<-function(locations, observations, FEMbasis, lambd
   {
     BC$BC_values<-as.vector(BC$BC_values)
   }
-  
+
   ## Set propr type for correct C++ reading
   locations <- as.matrix(locations)
   storage.mode(locations) <- "double"
@@ -64,29 +97,32 @@ CPP_smooth.manifold.FEM.basis<-function(locations, observations, FEMbasis, lambd
   storage.mode(BC$BC_values)  <- "double"
   GCV <- as.integer(GCV)
   storage.mode(GCV) <- "integer"
-  
+
   storage.mode(nrealizations) <- "integer"
   storage.mode(GCVMETHOD) <- "integer"
-  
+
   ## Call C++ function
-  bigsol <- .Call("regression_Laplace", locations, data, FEMbasis$mesh, FEMbasis$mesh$order, mydim, ndim, lambda, covariates,
-                  incidence_matrix, BC$BC_indices, BC$BC_values, GCV, GCVMETHOD, nrealizations, package = "fdaPDE")
-  
+
+
+  bigsol <- .Call("regression_Laplace", locations, time_locations, data, FEMbasis$mesh, time_mesh, FEMbasis$order,
+                  mydim, ndim, lambdaS, lambdaT, covariates, incidence_matrix, BC$BC_indices, BC$BC_values, FLAG_MASS, FLAG_PARABOLIC,
+                  IC, GCV, GCVMETHOD, nrealizations, PACKAGE = "fdaPDEtime")
+
   return(bigsol)
 }
 
 CPP_eval.manifold.FEM = function(FEM, locations, incidence_matrix, redundancy, ndim, mydim)
 {
   FEMbasis = FEM$FEMbasis
-  
+
   # C++ function for manifold works with vectors not with matrices
-  
+
   FEMbasis$mesh$triangles=c(t(FEMbasis$mesh$triangles))
   FEMbasis$mesh$nodes=c(t(FEMbasis$mesh$nodes))
-  
+
   #NEW: riporto shift indici in R
   FEMbasis$mesh$triangles=FEMbasis$mesh$triangles-1
-  
+
   # Imposing types, this is necessary for correct reading from C++
   ## Set proper type for correct C++ reading
   locations <- as.matrix(locations)
@@ -102,15 +138,14 @@ CPP_eval.manifold.FEM = function(FEM, locations, incidence_matrix, redundancy, n
   storage.mode(mydim) <- "integer"
   storage.mode(locations) <- "double"
   storage.mode(redundancy) <- "integer"
-  
+
   #Calling the C++ function "eval_FEM_fd" in RPDE_interface.cpp
   evalmat = matrix(0,max(nrow(locations),nrow(incidence_matrix)),ncol(coeff))
   for (i in 1:ncol(coeff)){
     evalmat[,i] <- .Call("eval_FEM_fd", FEMbasis$mesh, locations, incidence_matrix, coeff[,i],
                          FEMbasis$order, redundancy, mydim, ndim, package = "fdaPDE")
   }
-  
+
   #Returning the evaluation matrix
   evalmat
 }
-
